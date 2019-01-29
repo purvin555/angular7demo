dc704ba738c64e217d111a3b44e457b7830aaadf
purvin <purvin555@hotmail.com>
2019-01-29 17:52:00 +0530

    app name changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to benefitsconfig!');
+    expect(page.getTitleText()).toEqual('Welcome to Purvin Angular 7 Demo');
   });
 
   afterEach(async () => {
